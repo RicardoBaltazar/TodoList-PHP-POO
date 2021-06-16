6bd8c3da02515824e4179e045ee4b046ea8c00fa
Ricardo Baltazar <ricardo.baltazar04@gmail.com>
2021-06-16 19:36:59 -0300

    Criando Classes e Objetos

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO</title>
+</head>
+<body>
+    <?php
+    require_once './Class/Caneta.php';
 
-$pageLogin = file_get_contents("login.php");
-echo $pageLogin;
+    $caneta1 = new Caneta;
+    $caneta1->cor = 'Azul';
+    $caneta1->ponta = 0.5;
+    $caneta1->tampada = true;
+    $caneta1->destampar();
+    $caneta1->rabiscar();
 
-?>
+
+    ?>
+</body>
+</html>
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-unset($_SESSION['name']);
-unset($_SESSION['email']);
-unset($_SESSION['password']);
-header('location:index.php');
-
-?>
